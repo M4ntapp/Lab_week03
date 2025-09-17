bd6309c11a7d099204d2436a4edcaf84c1b131cd
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-09-17 19:36:17 +0700

    “Commit No. 1 – add fragment lifecyle logging”

---
+++ app/src/main/java/com/example/lab_week03/MainActivity.kt
@@ -0,0 +1,56 @@
+// Pastikan baris package ini sesuai dengan proyek Anda
+package com.example.lab_week03
+
+import android.os.Bundle
+import android.util.Log // <-- Import yang dibutuhkan untuk logging
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        // Pastikan ID di sini sesuai dengan ID root layout di activity_main.xml
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Menambahkan log untuk onCreate
+        Log.d(TAG, "onCreate")
+    }
+
+    // Menambahkan semua fungsi logging siklus hidup di bawah ini
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    // Mendeklarasikan TAG untuk kelas MainActivity
+    companion object {
+        private const val TAG = "MainActivity"
+    }
