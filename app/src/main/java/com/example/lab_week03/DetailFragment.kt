a627ee055917872918048c8d3b0eb11492bf14b0
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-09-17 20:52:25 +0700

    Commit No.2 - add coffee list & detail fragment

---
+++ app/src/main/java/com/example/lab_week03/DetailFragment.kt
@@ -0,0 +1,58 @@
+package com.example.lab_week03
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val coffeeId = arguments?.getInt(COFFEE_ID) ?: 0
+
+        setCoffeeData(coffeeId, view)
+    }
+
+    private fun setCoffeeData(id: Int, view: View) {
+        val coffeeTitle: TextView = view.findViewById(R.id.coffee_title)
+        val coffeeDesc: TextView = view.findViewById(R.id.coffee_desc)
+
+        when (id) {
+            R.id.affogato -> {
+                coffeeTitle.text = getString(R.string.affogato_title)
+                coffeeDesc.text = getString(R.string.affogato_desc)
+            }
+            R.id.americano -> {
+                coffeeTitle.text = getString(R.string.americano_title)
+                coffeeDesc.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                coffeeTitle.text = getString(R.string.latte_title)
+                coffeeDesc.text = getString(R.string.latte_desc)
+            }
+        }
+    }
+
+    companion object {
+        private const val COFFEE_ID = "COFFEE_ID"
+
+        fun newInstance(coffeeId: Int): DetailFragment {
+            val fragment = DetailFragment()
+            val args = Bundle()
+            args.putInt(COFFEE_ID, coffeeId)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
--- app/src/main/java/com/example/lab_week03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week03/MainActivity.kt
@@ -1,56 +1,42 @@
-// Pastikan baris package ini sesuai dengan proyek Anda
 package com.example.lab_week03
 
 import android.os.Bundle
-import android.util.Log // <-- Import yang dibutuhkan untuk logging
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+interface CoffeeListener {
+    fun onSelected(id: Int)
+}
 
+class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        // Pastikan ID di sini sesuai dengan ID root layout di activity_main.xml
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Menambahkan log untuk onCreate
-        Log.d(TAG, "onCreate")
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, ListFragment())
+                .commit()
+        }
     }
 
-    // Menambahkan semua fungsi logging siklus hidup di bawah ini
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
+    override fun onSelected(id: Int) {
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
+        val detailFragment = DetailFragment.newInstance(id)
 
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, detailFragment)
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
+            .addToBackStack(null)
+            .commit()
     }
-
-    // Mendeklarasikan TAG untuk kelas MainActivity
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+}
