8d068ec242dc20a4a5c1067a825f063a291e27d9
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-09-17 21:04:30 +0700

    “Commit No. 3 – use dynamic fragment”

--- app/src/main/java/com/example/lab_week03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week03/DetailFragment.kt
@@ -16,43 +16,47 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val coffeeId = arguments?.getInt(COFFEE_ID) ?: 0
 
-        setCoffeeData(coffeeId, view)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+
+
+        setCoffeeData(coffeeId)
     }
 
-    private fun setCoffeeData(id: Int, view: View) {
-        val coffeeTitle: TextView = view.findViewById(R.id.coffee_title)
-        val coffeeDesc: TextView = view.findViewById(R.id.coffee_desc)
+    private fun setCoffeeData(id: Int) {
+
+        val coffeeTitle: TextView? = view?.findViewById(R.id.coffee_title)
+        val coffeeDesc: TextView? = view?.findViewById(R.id.coffee_desc)
 
         when (id) {
             R.id.affogato -> {
-                coffeeTitle.text = getString(R.string.affogato_title)
-                coffeeDesc.text = getString(R.string.affogato_desc)
+                coffeeTitle?.text = getString(R.string.affogato_title)
+                coffeeDesc?.text = getString(R.string.affogato_desc)
             }
             R.id.americano -> {
-                coffeeTitle.text = getString(R.string.americano_title)
-                coffeeDesc.text = getString(R.string.americano_desc)
+                coffeeTitle?.text = getString(R.string.americano_title)
+                coffeeDesc?.text = getString(R.string.americano_desc)
             }
             R.id.latte -> {
-                coffeeTitle.text = getString(R.string.latte_title)
-                coffeeDesc.text = getString(R.string.latte_desc)
+                coffeeTitle?.text = getString(R.string.latte_title)
+                coffeeDesc?.text = getString(R.string.latte_desc)
             }
         }
     }
+
 
     companion object {
         private const val COFFEE_ID = "COFFEE_ID"
 
-        fun newInstance(coffeeId: Int): DetailFragment {
-            val fragment = DetailFragment()
-            val args = Bundle()
-            args.putInt(COFFEE_ID, coffeeId)
-            fragment.arguments = args
-            return fragment
-        }
+        fun newInstance(coffeeId: Int) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(COFFEE_ID, coffeeId)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/lab_week03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week03/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 
 interface CoffeeListener {
     fun onSelected(id: Int)
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onSelected(id: Int) {
 
         val detailFragment = DetailFragment.newInstance(id)
+
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, detailFragment)
